997ede6cbda2e8705ad48f66a322c524b0fc38d5
Kiran <dev.apptiks@gmail.com>
2023-12-27 10:53:05 -0600

    logger added

--- src/main/java/com/rgbiryani/registry/serviceregistry/ServiceRegistryApplication.java
+++ src/main/java/com/rgbiryani/registry/serviceregistry/ServiceRegistryApplication.java
@@ -5,12 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
 @EnableEurekaServer
 @EnableFeignClients
+@Slf4j
 public class ServiceRegistryApplication {
 
 	public static void main(String[] args) {
+		System.out.println("<<<<<< Inside SB main method >>>>>>");
 		SpringApplication.run(ServiceRegistryApplication.class, args);
 	}
 
